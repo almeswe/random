378946cd4f49c982d71d94981b9f365e7e342d28
Almeswe <52577119+Almeswe@users.noreply.github.com>
2021-05-13 18:22:31 +0300

    Update main.cpp

--- random/main.cpp
+++ random/main.cpp
@@ -4,30 +4,30 @@
 #define uint  unsigned int
 #define ulong unsigned long long
 
-#define for_i_to_size(size)				  for (size_t i = 1; i <= size; i++)
-#define cout_data_endl(data)			  std::cout << data << std::endl;
-#define cout_sequence_of_data(size, data) for_i_to_size(size) cout_data_endl(data)
+#define FOR_I_TO_SIZE(size)			 for (size_t i = 1; i <= size; i++)
+#define COUT_DATA_ENDL(data)		 std::cout << data << std::endl;
+#define COUT_SEQ_OF_DATA(size, data) FOR_I_TO_SIZE(size) COUT_DATA_ENDL(data)
 
 void gen64_test(size_t size = 25)
 {
-	cout_sequence_of_data(size, random64())
+	COUT_SEQ_OF_DATA(size, random64())
 }
 void gen64_range_test(ulong from, ulong to, size_t size = 25)
 {
-	cout_sequence_of_data(size, randrange64(from, to))
+	COUT_SEQ_OF_DATA(size, randrange64(from, to))
 }
 
 void gen32_range_test(uint from, uint to, size_t size = 25)
 {
-	cout_sequence_of_data(size, randrange32(from, to))
+	COUT_SEQ_OF_DATA(size, randrange32(from, to))
 }
 void gen32_test(size_t size = 25)
 {
-	cout_sequence_of_data(size, random32())
+	COUT_SEQ_OF_DATA(size, random32())
 }
 
 int main(int argc, char** argv)
 {
-	gen64_range_test(0, 10, 123);
+	gen64_test(15);
 	return 0;
 }
